265342d84c20c908aafdc8212f8a67dcaf697c2a
丹青阁 <ganyu1016@gmail.com>
2018-11-18 13:24:57 +0800

    2

---
+++ src/main/java/com/yucode/ci/Application.java
@@ -0,0 +1,18 @@
+package com.yucode.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Yu.Gan
+ * @desc:
+ * @date: 2018/11/18 13:21
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
---
+++ src/main/java/com/yucode/ci/MainController.java
@@ -0,0 +1,20 @@
+package com.yucode.ci;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Yu.Gan
+ * @desc:
+ * @date: 2018/11/18 13:22
+ */
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+  @RequestMapping
+  public String hello() {
+    return "Hello World!";
+  }
+
+}
